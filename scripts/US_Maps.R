68cabaa4124ef50df30c7b9751662918c05c127a
Charles Martin <martin.charles@gmail.com>
2016-09-27 09:56:23 -0400

    Something's wrong with the map. Lousiana and California should be the darkest states

--- scripts/US_Maps.R
+++ scripts/US_Maps.R
@@ -1,17 +1,23 @@
 rm(list = ls())
 
+options(tibble.print_max = Inf)
+
 library(maps)
 data(state)
 
 load("data/US_Diversity.RData")
 
-range01 <- function(x){(x-min(x))/(max(x)-min(x))}
+range01 <- function(x){(x - min(x))/(max(x) - min(x))}
 
-ramp <- colorRamp(c("white","navyblue"))
+ramp <- colorRamp(c("white","black"))
 
 map(
   database = "state",
   regions = state.name,
-  col=rgb(ramp(range01(res$alpha)), maxColorValue = 255),
+  col = rgb(ramp(range01(res$alpha)), maxColorValue = 255),
   fill = TRUE
 )
+
+res[order(res$alpha, decreasing = TRUE),]
+
+# vs http://www.biodiversitymapping.org/USA_birds.htm#thumb
