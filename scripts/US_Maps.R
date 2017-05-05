7919df63cbc52afe0854f1325813eede2f77eb90
Charles Martin <martin.charles@gmail.com>
2017-05-05 14:42:15 -0400

    Clean up the map generating code

--- scripts/US_Maps.R
+++ scripts/US_Maps.R
@@ -5,6 +5,7 @@
 rm(list = ls())
 library(stringr)
 library(ggplot2)
+library(purrr)
 
 load("data/US_Diversity.RData")
 
@@ -15,70 +16,25 @@ all_states <- all_states[all_states$region != "district of columbia",]
 
 Total <- merge(all_states, res, by = "region")
 
-p1 <- ggplot()
-p1 <- p1 + geom_polygon(
-  data = Total,
-  aes(x = long, y = lat, group = group, fill = Total$alpha),
-  colour = "white"
-) +
-  scale_fill_continuous(low = "thistle2", high = "darkred", guide = "colorbar")
+pwalk(list(
+  div_var = c("alpha","gamma", "beta"),
+  fig_name_suffix = c("a","b","c"),
+  high_color = c("darkred", "darkgreen", "darkblue")
+),function(div_var,fig_name_suffix,high_color) {
+  ggplot() +
+    geom_polygon(
+    data = Total,
+    aes(x = long, y = lat, group = group, fill = Total[,div_var]),
+    colour = "white") +
+    scale_fill_continuous(low = "thistle2", high = high_color, guide = "colorbar") +
+    theme_bw() +
+    labs(fill = str_to_title(div_var), x = "", y = "") +
+    scale_y_continuous(breaks = c()) +
+    scale_x_continuous(breaks = c()) +
+    theme(panel.border = element_blank()) +
+    ggtitle(fig_name_suffix) + theme(plot.title = element_text(hjust = 0))
 
-p1 <- p1+ theme_bw() + labs(fill = "Alpha"
-                            #,title = "Bird diversity in the continental US"
-                            ,x = "", y = "")
-p1 <- p1 + scale_y_continuous(breaks = c()) +
-  scale_x_continuous(breaks = c()) +
-  theme(panel.border = element_blank())
+  ggsave(paste0("results/Fig1", fig_name_suffix ,".eps"), width = 0.66*8, height = 0.33*11)
 
-p1 <- p1 + ggtitle('a') + theme(plot.title=element_text(hjust=0))
-p1
-ggsave("results/Fig1a.eps")
-
-#########
-
-p2 <- ggplot()
-p2 <- p2 + geom_polygon(
-  data = Total,
-  aes(x = long, y = lat, group = group, fill = Total$gamma),
-  colour = "white"
-) +
-  scale_fill_continuous(low = "thistle2", high = "darkgreen", guide = "colorbar")
-
-p2 <- p2 + theme_bw() + labs(fill = "Gamma"
-                            #,title = "Bird diversity in the continental US"
-                            , x = "", y = "")
-p2 <- p2 + scale_y_continuous(breaks = c()) +
-  scale_x_continuous(breaks = c()) +
-  theme(panel.border = element_blank())
-
-p2 <- p2 + ggtitle('b') + theme(plot.title=element_text(hjust=0))
-p2
-ggsave("results/Fig1b.eps")
-
-
-########
-
-p3 <- ggplot()
-p3 <- p3 + geom_polygon(
-  data = Total,
-  aes(x = long, y = lat, group = group, fill = Total$beta),
-  colour = "white"
-) +
-  scale_fill_continuous(low = "thistle2", high = "darkblue", guide = "colorbar")
-
-p3 <- p3 + theme_bw() + labs(fill = "Beta"
-                            #,title = "Bird diversity in the continental US"
-                            , x = "", y = "")
-p3 <- p3 + scale_y_continuous(breaks = c()) +
-  scale_x_continuous(breaks = c()) +
-  theme(panel.border = element_blank())
-
-p3 <- p3 + ggtitle('c') + theme(plot.title=element_text(hjust=0))
-p3
-ggsave("results/Fig1c.eps")
-
-
-#######
-
-# source("lib/multiplot.R")
-# multiplot(p1, p2, p3, cols = 1)
+}
+)
