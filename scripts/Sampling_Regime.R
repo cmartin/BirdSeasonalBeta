4bf1add1e43912877b2a25616c08fcbc2dbcb69a
Charles Martin <martin.charles@gmail.com>
2017-05-08 13:33:26 -0400

    Correct equal spacing between sampling regimes

--- scripts/Sampling_Regime.R
+++ scripts/Sampling_Regime.R
@@ -35,11 +35,11 @@ ranks <- res %>%
   ungroup
 
 ranks %>%
-  ggplot(aes(x = n_checklists_per_week^.25, y = rank)) +
+  ggplot(aes(x = as.numeric(as.factor(n_checklists_per_week)), y = rank)) +
   geom_line(aes(color = location), size = 1) +
   scale_y_reverse() +
   scale_x_continuous(
-    breaks = unique(res$n_checklists_per_week)^.25,
+    breaks = as.numeric(as.factor(unique(res$n_checklists_per_week))),
     labels = unique(res$n_checklists_per_week)
   ) +
   labs(
