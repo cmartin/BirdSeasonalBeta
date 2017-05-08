77b5718b10619c8aee24b5d26b7badf31f0733ff
Charles Martin <martin.charles@gmail.com>
2017-05-08 12:12:37 -0400

    Begin the exploration of the effects of the sampling regime

--- scripts/Sampling_Regime.R
+++ scripts/Sampling_Regime.R
@@ -1,14 +1,40 @@
 library(tidyverse)
 library(stringr)
-load("data/US_Diversity.RData")
 
+res <- read_csv("data/Simulation_Results.csv")
+
+se <- function(x) {
+  sd(x)/sqrt(length(x))
+}
+
+View(
 res %>%
+  group_by(location, n_checklists_per_week) %>%
+  summarise_each(
+    funs(mean,se),
+    alpha,
+    beta,
+    gamma
+  )
+)
+
+ranks <- res %>%
+  group_by(location, n_checklists_per_week) %>%
+  summarise_each(
+    funs(mean),
+    alpha,
+    beta,
+    gamma
+  ) %>%
+  ungroup() %>%
   mutate(location = str_replace(location,"US-","")) %>%
   gather(key = "diversity_metric", value = "value", alpha,gamma,beta) %>%
   group_by(n_checklists_per_week,diversity_metric) %>%
   arrange(desc(value)) %>%
   mutate(rank = row_number()) %>%
-  ungroup %>%
+  ungroup
+
+ranks %>%
   ggplot(aes(x = n_checklists_per_week^.25, y = rank)) +
   geom_line(aes(color = location), size = 1) +
   scale_y_reverse() +
@@ -25,3 +51,14 @@ res %>%
   facet_wrap(~diversity_metric) +
   theme(legend.position = "bottom") +
   guides(color = guide_legend(nrow = 3))
+
+rank_table <- ranks %>%
+  select(location, n_checklists_per_week, diversity_metric, rank) %>%
+  filter(diversity_metric == "beta") %>%
+  spread(key = n_checklists_per_week, value = rank)
+
+View(
+  rank_table
+)
+
+rank_table %>% select(`2`:`2048`) %>% cor
