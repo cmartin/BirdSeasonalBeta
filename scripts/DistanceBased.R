1d3938b66e487cde921ccbdb1f5c1d6cdc8e230a
Charles Martin <martin.charles@gmail.com>
2017-05-09 14:36:51 -0400

    A quick map of the distance-based approach results

---
+++ scripts/DistanceBased.R
@@ -0,0 +1,52 @@
+library(tidyverse)
+library(memoise)
+library(rebird)
+library(stringr)
+library(vegan)
+source("lib/EBird_Tools.R")
+
+distances <- map_df(
+  state.abb,
+  function(x) {
+    f <- calculate_frequencies(x)
+
+    # Calculate distance matrix
+    m <- as.matrix(vegdist(t(f), method = "horn")) # Which is Morista-Horn
+
+    # Extract distance to neighboring-week
+    d <- diag(m[-1,])
+    #plot(d, type = "l")
+    list(
+      d = mean(d),
+      location = x
+    )
+  }
+)
+
+### Quick map
+
+all_states <- map_data("state")
+Total <- merge(
+  all_states[all_states$region != "district of columbia",],
+  distances %>%
+    merge(
+      tibble(
+        region = str_to_lower(state.name),
+        location = state.abb
+      )
+    )
+)
+
+ggplot() +
+  geom_polygon(
+    data = Total,
+    aes(x = long, y = lat, group = region, fill = d),
+    colour = "white"
+  ) +
+  scale_fill_continuous(low = "thistle2", high = "blue", guide = "colorbar") +
+  theme_bw() +
+  labs(fill = str_to_title("Horn-Morista\nmean"), x = "", y = "") +
+  scale_y_continuous(breaks = c()) +
+  scale_x_continuous(breaks = c()) +
+  theme(panel.border = element_blank()) +
+  theme(plot.title = element_text(hjust = 0))
