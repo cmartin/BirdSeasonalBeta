6da313daf0adc2e0aff779c03e67afb0aeeec420
Charles Martin <martin.charles@gmail.com>
2017-05-10 10:49:01 -0400

    Using the actual Horn index instead of Morisita-Horn

--- scripts/DistanceBased.R
+++ scripts/DistanceBased.R
@@ -5,17 +5,24 @@ library(stringr)
 library(vegan)
 source("lib/EBird_Tools.R")
 source("lib/Monthly_Climate.R")
+source("lib/Horn.R")
 
 distances <- map_df(
   state.abb,
   function(x) {
     f <- calculate_frequencies(x)
 
-    # Calculate distance matrix
-    m <- as.matrix(vegdist(t(f), method = "horn")) # Which is Morista-Horn
+    # # Calculate distance matrix
+    # m <- as.matrix(vegdist(t(f), method = "horn")) # Which is Morista-Horn
+    # # Extract distance to neighboring-week
+    # d <- diag(m[-1,])
 
-    # Extract distance to neighboring-week
-    d <- diag(m[-1,])
+    n <- ncol(f)
+    d <- c(rep(NA,n - 1))
+    for (i in 1:(n - 1)) {
+      d[i] <- 1 - horn2(f[,i],f[,i + 1])
+    }
+
     #plot(d, type = "l")
     list(
       d = mean(d),
@@ -73,7 +80,7 @@ ggplot(Total) +
   ) +
   scale_fill_continuous(low = "thistle2", high = "blue", guide = "colorbar") +
   theme_bw() +
-  labs(fill = str_to_title("Morisita-Horn"), x = "", y = "") +
+  labs(fill = str_to_title("Horn"), x = "", y = "") +
   scale_y_continuous(breaks = c()) +
   scale_x_continuous(breaks = c()) +
   theme(panel.border = element_blank()) +
@@ -89,3 +96,4 @@ distances %>%
   geom_point() +
   geom_smooth() +
   facet_wrap(~var, scales = "free_x")
+
