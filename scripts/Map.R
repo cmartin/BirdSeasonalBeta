b26032c9f8139fd783112b4b97f5b235e0ff4949
Charles Martin <martin.charles@gmail.com>
2017-05-17 09:30:23 -0400

    Fig. 1 back to a single map

--- scripts/Map.R
+++ scripts/Map.R
@@ -9,32 +9,6 @@ Total <- merge(
   distances
 ) %>% arrange(order)
 
-p1 <- ggplot(Total) +
-  geom_polygon(
-    aes(x = long, y = lat, group = group, fill = alpha),
-    colour = "white"
-  ) +
-  scale_fill_continuous(low = "thistle2", high = "darkred", guide = "colorbar") +
-  theme_bw() +
-  labs(fill = "", x = "", y = "") +
-  scale_y_continuous(breaks = c()) +
-  scale_x_continuous(breaks = c()) +
-  theme(panel.border = element_blank(), legend.text.align = 3) +
-  annotate("text", label = "(a)", x = -128, y = 49, vjust = 1, size = 5)
-
-p2 <- ggplot(Total) +
-  geom_polygon(
-    aes(x = long, y = lat, group = group, fill = temp_range),
-    colour = "white"
-  ) +
-  scale_fill_continuous(low = "thistle2", high = "darkblue", guide = "colorbar") +
-  theme_bw() +
-  labs(fill = "", x = "", y = "") +
-  scale_y_continuous(breaks = c()) +
-  scale_x_continuous(breaks = c()) +
-  theme(panel.border = element_blank()) +
-  annotate("text", label = "(b)", x = -128, y = 49, vjust = 1, size = 5)
-
 p3 <- ggplot(Total) +
   geom_polygon(
     aes(x = long, y = lat, group = group, fill = d),
@@ -45,15 +19,7 @@ p3 <- ggplot(Total) +
   labs(fill = "", x = "", y = "") +
   scale_y_continuous(breaks = c()) +
   scale_x_continuous(breaks = c()) +
-  theme(panel.border = element_blank()) +
-  theme(plot.title = element_text(hjust = 0)) +
-  annotate("text", label = "(c)", x = -128, y = 49, vjust = 1, size = 5)
+  theme(panel.border = element_blank())
 
-
-combined_plots <- ggarrange(
-  p1,p2,p3,
-  ncol = 1
-)
-
-ggsave(paste0("results/Fig1.eps"), combined_plots, width = 0.75*8, height = 0.33*11*3)
+ggsave(paste0("results/Fig1.eps"), p3, width = 0.75*8, height = 0.33*11)
 
