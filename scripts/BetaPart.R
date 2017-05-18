d83c5c981bf015ee1c7b30bb44dbf16acbfa3c66
Charles Martin <martin.charles@gmail.com>
2017-05-18 09:34:26 -0400

    Cleaning up the exploration of beta partitioning

--- scripts/BetaPart.R
+++ scripts/BetaPart.R
@@ -1,15 +1,37 @@
+# Exploring beta diversity components
 library(tidyverse)
 distances <- read_csv("data/distances.csv")
 
-plot(horn~bray,data = distances)
+# Bray-Curtis and Horn dissimilarity are mostly the same
+distances %>%
+  ggplot(aes(x = horn, y = bray)) +
+  geom_point()
+cor(distances$horn, distances$bray)
 
-# Exploring beta diversity components
-plot(turnover~nestedness,data = distances)
-plot(turnover~latitude,data = distances)
-plot(nestedness~latitude,data = distances)
-plot(bray~I(turnover+nestedness),data = distances)
+# In our dataset, turnover and nestedness are moderately correlated
+distances %>%
+  ggplot(aes(x = turnover, y = nestedness)) +
+  geom_point()
+cor(distances$turnover,distances$nestedness)
 
-plot(turnover~gamma,data = distances)
-plot(nestedness~gamma,data = distances)
-plot(turnover~temp_range,data = distances)
-plot(nestedness~temp_range,data = distances)
+db <- distances %>%
+  gather(key = part, value = value, turnover, nestedness)
+
+# Components don't change differently in the latitude gradient
+db %>%
+  ggplot(aes(y = value, x = latitude)) +
+  geom_point() +
+  geom_smooth() +
+  facet_wrap(~part, scales = "free_y")
+
+summary(step(lm(value~part*latitude,data = db)))
+
+# And relationships to the independent vars are very close to those of the Horn dissimilarity
+distances %>%
+  gather(key = part, value = div, turnover, nestedness) %>%
+  gather(key = explanatory_var, value = value, gamma, temp_range) %>%
+  ggplot(aes(x = value,y = div)) +
+  geom_point() +
+  geom_smooth(method = "lm", formula = y~poly(x,2)) +
+  facet_grid(part~explanatory_var, scales = "free")
+
