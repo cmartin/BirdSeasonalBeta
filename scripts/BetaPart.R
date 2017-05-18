5c2ff0323cda20c4c29a6d941a270ed4d06a424e
Charles Martin <martin.charles@gmail.com>
2017-05-18 15:11:41 -0400

    So this concludes the results about the partitioning of beta diversity

--- scripts/BetaPart.R
+++ scripts/BetaPart.R
@@ -1,6 +1,6 @@
 # Exploring beta diversity components
 library(tidyverse)
-distances <- read_csv("data/distances.csv")
+distances <- read_csv("data/distances.csv") %>% filter(complete.cases(.))
 
 # Bray-Curtis and Horn dissimilarity are mostly the same
 distances %>%
@@ -33,5 +33,39 @@ distances %>%
   ggplot(aes(x = value,y = div)) +
   geom_point() +
   geom_smooth(method = "lm", formula = y~poly(x,2)) +
-  facet_grid(part~explanatory_var, scales = "free")
+  facet_grid(part~explanatory_var, scales = "free_x")
+
+library(MuMIn)
+options(na.action = "na.fail")
+
+
+m <- lm(turnover~gamma+I(gamma^2)+temp_range+I(temp_range^2), data = distances)
+summary(m)
+d <- dredge(m, subset = dc(gamma, I(gamma^2)) + dc(temp_range,I(temp_range^2)))
+d
+importance(d)
+
+m2 <- lm(nestedness~gamma+I(gamma^2)+temp_range+I(temp_range^2), data = distances)
+summary(m2)
+d2 <- dredge(m2, subset = dc(gamma, I(gamma^2)) + dc(temp_range,I(temp_range^2)))
+d2
+importance(d2)
+
+transf <-
+  distances %>%
+  mutate_each(
+    funs(scale),
+    turnover,
+    nestedness,
+    gamma,
+    temp_range,
+    horn
+  )
+
+m_h <- lm(horn ~ gamma+temp_range+I(temp_range^2), data = transf)
+m_t <- lm(turnover ~ gamma+temp_range+I(temp_range^2), data = transf)
+m_n <- lm(nestedness ~ gamma+temp_range+I(temp_range^2), data = transf)
+confint(m_h, -1)
+confint(m_t, -1)
+confint(m_n, -1)
 
